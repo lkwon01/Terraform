320bbd864c0bfcf23972c8d56632418fbce78f73
Laura Collins <lkwon01@yahoo.com>
2023-12-07 09:03:40 +0000

    Create Jenkins server and S3 bucket

--- jenkins/main.tf
+++ jenkins/main.tf
@@ -1,45 +1,18 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
-
-# Configure the AWS Provider
-provider "aws" {
-  region = var.myregion
-}
-
-variable "myregion" {
-    description = "this is where my infra will be deployed"
-    type = string
-    default = "us-east-1"
-}
-
-#Create EC2 resource
+#Create EC2 instance and boostrap the installation of Jenkins to the EC2 server.
 resource "aws_instance" "Mywebsite" {
   ami           = var.ami_id
   instance_type = var.instance
+  subnet_id     = var.subnet_id
+  vpc_security_group_ids = [aws_security_group.ec2.security_group.id]
+  ke_name       = var.key_name.pem
   user_data              = file("jenkins_install.sh")
+  
 
   tags = {
-    Name = "Jenkins"
+    Name = "my_website"
   }
 }
 
-variable "ami_id" {
-    description = "this is my ami id"
-    type = string
-    default = "ami-0230bd60aa48260c6"
-}
-
-variable "instance" {
-    description = "this is my instance type"
-    type = string
-    default = "t2.micro"
-}
 #Create s3 bucket
 
 
@@ -52,21 +25,21 @@ resource "aws_s3_bucket" "Jenkins_Artifacts" {
   }
   }
   
-variable "mybucketname" {
-    description = "this is my instance type"
-    type = string
-    default = "my-tf-bucket-hyejung"
-    }
+  
+resource "aws_s3_bucket_acl" "my-jenkins-bucket" {
+  bucket = aws_s3_bucket.bucket.id
+  acl = "private"
+}
     
 # Create security group
 
 resource "aws_security_group" "jenkins_sg" {
   name        = "jenkins"
-  description = "Allow jenkins inbound traffic"
+  description = "Allow jenkins inbound traffic on ports 8080 and 22"
   vpc_id      = var.myvpc
 
   ingress {
-    description      = "Jenkins port from my IP"
+    description      = "allow Jenkins traffic from the internet"
     from_port        = 8080
     to_port          = 8080
     protocol         = "tcp"
@@ -74,7 +47,24 @@ resource "aws_security_group" "jenkins_sg" {
    
   }
   
-
+   ingress {
+    description      = "allow http traffic from the internet"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+   
+  }
+  
+  ingress {
+    description      = "allow ssh traffic from the internet"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+   
+  }
+  
   egress {
     from_port        = 0
     to_port          = 0
@@ -83,11 +73,7 @@ resource "aws_security_group" "jenkins_sg" {
   
   }
 
+  tags = {
+    Name = "jenkins security group"
   }
-
-
-variable "myvpc" {
-    description = "this is my vpc id"
-    type = string
-    default = "vpc-0d7c3d48634090867"
-    }
+  }
