b57e425e4225393267adc7efed9c643a41be98b9
Laura Collins <lkwon01@yahoo.com>
2023-12-15 05:26:46 +0000

    remote

---
+++ Remote/terraform.tf
@@ -0,0 +1,35 @@
+terraform {
+  backend "remote" {
+    hostname     = "app.terraform.io"
+    organization = "lexa"
+
+    workspaces {
+      name = "lexa"
+    }
+  }
+
+
+required_version = ">= 1.0.0"
+required_providers {
+  aws = {
+    source  = "hashicorp/aws"
+    version = "~> 5.0"
+  }
+  http = {
+    source  = "hashicorp/http"
+    version = "2.1.0"
+  }
+  random = {
+    source  = "hashicorp/random"
+    version = "3.1.0"
+  }
+  local = {
+    source  = "hashicorp/local"
+    version = "2.1.0"
+  }
+  tls = {
+    source  = "hashicorp/tls"
+    version = "3.1.0"
+  }
+}
+}
