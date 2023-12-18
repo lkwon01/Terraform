fe67d9d5c79d43af807c5e405d4aa131230d2dd9
Laura Collins <lkwon01@yahoo.com>
2023-12-18 05:12:16 +0000

    vault

---
+++ vault/vault/main.tf
@@ -0,0 +1,13 @@
+provider "vault"
+  address ="http://127.0.0.1:8200"
+  token =""
+  
+data "valut_generic_sercret" "phone_number" {
+
+  path = "secret/app"
+  
+}
+
+output "phone_number" {
+    value = data.vault_generic_secret.phone_number
+}
